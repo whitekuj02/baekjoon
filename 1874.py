b348afabb6d7c91c85dfaf9b309b86fd8a84ae83
whitekuj02 <whitekuj02@naver.com>
2022-12-25 04:19:12 +0900

    백준

--- 1874.py
+++ 1874.py
@@ -15,10 +15,31 @@ def input(a):
     stack_forward.append("+")
 
 def output(a):
-    stack.pop(a)
-    stack_forward.append("-")
+    pop_data = stack.pop()
+    if pop_data == a:
+        stack_forward.append("-")
+    else:
+        stack.append(a)
+        stack_forward.append("NO")
 
-while stack_data > num:
+data_comduct = 0
+success = 1
+while data_comduct < len(data):
+    if data[data_comduct] >= stack_data:
+        while data[data_comduct] >= stack_data:
+            input(stack_data)
+            stack_data += 1
+        output(data[data_comduct])
+    elif data[data_comduct] < stack_data:
+        output(data[data_comduct])
 
+    if stack_forward[len(stack_forward)-1] == "NO":
+        success = 0
+        print("NO")
+        break
+    data_comduct += 1
 
+if success == 1:
+    for i in range(0, len(stack_forward)):
+        print(stack_forward[i])
 
